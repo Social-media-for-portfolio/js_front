985081c9c64b85d223f9d25b5ce87c8bfc752374
emir4ik-code <emirahmetov@gmail.com>
2022-10-19 18:02:19 -0400

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../sections/Navbar'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <div>Home Page</div>
+    </>
+  )
+}
+
+export default Home
---
+++ src/sections/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./navbar.css"
+
+const Navbar = () => {
+  return (
+    <nav className="d-flex justify-content-between navbar align-items-center">
+      <div className="d-flex justify-content-between mx-2">
+        <h6>About Us</h6>
+      </div>
+      <div className="d-flex justify-content-between align-items-center"></div>
+    </nav>
+  );
+};
+
+export default Navbar;
