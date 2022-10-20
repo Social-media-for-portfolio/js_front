8bd1d766adddb80871ecdbb89eff1c6cd91d8c22
emir4ik-code <emirahmetov@gmail.com>
2022-10-20 17:41:21 -0400

    box shadow experiment for posts

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -14,7 +14,7 @@ const Post = ({
   comments = 0,
 }) => {
   return (
-    <div className="my-5 w-25">
+    <div className="my-5 w-25 post">
       <div className="d-flex align-items-center">
         <img src={avatar} className="post-avatar" />
         <div className="d-flex flex-column mx-3">
@@ -26,14 +26,16 @@ const Post = ({
         <p className="px-3">{body}</p>
         {src && <img src={src} className="w-100 h-100" />}
       </div>
+      <div className = "mt-5">
       <div className="d-flex justify-content-between">
         <div className="d-flex">
           <p>{likes}</p>
           <GrLike className="mx-2 fs-4" />
         </div>
-        <div className="d-flex justify-content-between">
+        <div className="d-flex justify-content-between ">
           <p>{comments + " "}Comments</p>
           <FaRegCommentDots className="mx-2 fs-4" />
+        </div>
         </div>
       </div>
     </div>
