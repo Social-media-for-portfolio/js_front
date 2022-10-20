58c3cd34abdd6b6125a8bcf4cdb8520ce2a88b95
emir4ik-code <emirahmetov@gmail.com>
2022-10-20 16:39:34 -0400

    likes and comments metrics

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,27 +1,41 @@
 import React from "react";
-import "./post.css"
+import "./post.css";
+import { GrLike } from "react-icons/gr";
+import { FaRegCommentDots } from "react-icons/fa";
+import { DateTime } from "luxon";
 
-const Post = ({ src, avatar, username, dateTime, body }) => {
+const Post = ({
+  src,
+  avatar,
+  username,
+  dateTime,
+  body,
+  likes = 0,
+  comments = 0,
+}) => {
   return (
-    <div className = "my-5">
+    <div className="my-5 w-25">
       <div className="d-flex align-items-center">
-        <img src={avatar} className = "post-avatar"/>
+        <img src={avatar} className="post-avatar" />
         <div className="d-flex flex-column mx-3">
           <h2>{username}</h2>
-          <p>{dateTime}</p>
+          <p>{DateTime.fromISO(dateTime).toRelative()}</p>
         </div>
       </div>
       <div>
         <p className="px-3">{body}</p>
         {src && <img src={src} className="w-100 h-100" />}
       </div>
-      {/* <div className="d-flex justify-content-between">
+      <div className="d-flex justify-content-between">
         <div className="d-flex">
-          {likes && <GrLike className="mx-2 fs-4" />}
           <p>{likes}</p>
+          <GrLike className="mx-2 fs-4" />
         </div>
-        <p>{comments} Comments</p>
-      </div> */}
+        <div className="d-flex justify-content-between">
+          <p>{comments + " "}Comments</p>
+          <FaRegCommentDots className="mx-2 fs-4" />
+        </div>
+      </div>
     </div>
   );
 };
