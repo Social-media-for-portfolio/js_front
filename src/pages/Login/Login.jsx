adae631ba562fbdc0e05f1a7263e1e11de7aab1c
emir4ik-code <emirahmetov@gmail.com>
2022-10-21 22:21:00 -0400

    login/signup layout start

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useContext } from "react";
 import Navbar from "../../sections/Navbar";
+import Footer from "../../sections/Footer";
+import Logo from "../../assets/logo.jpg"
 import AuthContext from "../../context/authContext/AuthContext";
 
 const Login = () => {
@@ -47,27 +49,38 @@ const Login = () => {
   };
 
   return (
-    <>
+    <div className="container-fluid px-0 mx-0">
       <Navbar />
-      <form
-        onSubmit={handleSubmit}
-        className="d-flex flex-column h-25 w-50 justify-content-between"
-      >
-        <input
-          onChange={handleChange}
-          type="email"
-          placeholder="Enter your email"
-          id="email"
-        />
-        <input
-          onChange={handleChange}
-          type="password"
-          placeholder="Enter your password"
-          id="password"
-        />
-        <button>Log in</button>
-      </form>
-    </>
+      <div>
+        <div className="row">
+          <div className="col-6">
+            <img src = {Logo} alt = "logo"/>
+          </div>
+          <div className="col-6 d-flex flex-column align-items-end">
+            <form
+              onSubmit={handleSubmit}
+              className="d-flex flex-column h-25 w-50 my-5 mx-5"
+            >
+              <input
+                onChange={handleChange}
+                type="email"
+                placeholder="Enter your email"
+                id="email"
+              />
+              <input
+                onChange={handleChange}
+                type="password"
+                placeholder="Enter your password"
+                id="password"
+              />
+              <button>Log in</button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
   );
 };
 
