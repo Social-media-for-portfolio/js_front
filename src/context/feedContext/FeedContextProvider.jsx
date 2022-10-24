e3da5d5260f921816bfcd86fd6e6af1338df8eca
emir4ik-code <emirahmetov@gmail.com>
2022-10-24 18:17:26 -0400

    render comment and like metrics properly after login

--- src/context/feedContext/FeedContextProvider.jsx
+++ src/context/feedContext/FeedContextProvider.jsx
@@ -11,66 +11,10 @@ const FeedContextProvider = ({ children }) => {
     setFeed,
     feedMetrics,
     setFeedMetrics,
-    commentMetrics, 
-    setCommentMetrics
+    commentMetrics,
+    setCommentMetrics,
   };
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-
-    //Get Post Metrics
-    const getPostMetrics = async () => {
-      const map = {};
-      const commentResponse = await fetch(
-        "http://localhost:5000/feed/comments/metrics",
-        {
-          method: "GET",
-          headers: { "Content-Type": "application/json", token: token },
-        }
-      );
-      const commentData = await commentResponse.json();
-      for (let commentMetric of commentData) {
-        map[commentMetric.post_id] = [parseInt(commentMetric.commentcount), 0];
-      }
-      const likeResponse = await fetch(
-        "http://localhost:5000/feed/posts/likes",
-        {
-          method: "GET",
-          headers: { "Content-Type": "application/json", token: token },
-        }
-      );
-      const likeData = await likeResponse.json();
-      for (let likeMetric of likeData) {
-        if (likeMetric.post_id in map) {
-          map[likeMetric.post_id][1] = parseInt(likeMetric.likecount);
-        } else {
-          map[likeMetric.post_id] = [0, parseInt(likeMetric.likecount)];
-        }
-      }
-      setFeedMetrics(map);
-    };
-    getPostMetrics();
-
-    //Get Comment Metrics
-
-    const getCommentMetrics = async () => {
-      const map = {};
-
-      const response = await fetch(
-        "http://localhost:5000/feed/comments/likes",
-        {
-          method: "GET",
-          headers: { "Content-Type": "application/json", token: token },
-        }
-      );
-      const commentLikesData = await response.json();
-      for (let commentLike of commentLikesData) {
-        map[commentLike.comment_id] = parseInt(commentLike.likecount);
-      }
-      setCommentMetrics(map);
-    };
-    getCommentMetrics();
-  }, []);
   return <FeedContext.Provider value={state}>{children}</FeedContext.Provider>;
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,7 +10,8 @@ import "./home.css";
 
 const Home = () => {
   const { userInfo, setUserInfo } = useContext(AuthContext);
-  const { feed, setFeed} = useContext(FeedContext);
+  const { feed, setFeed, setFeedMetrics, setCommentMetrics } =
+    useContext(FeedContext);
 
   const retrieveFeed = async () => {
     setFeed(await getAllPosts());
@@ -37,9 +38,53 @@ const Home = () => {
       />
     );
   });
+  const getPostMetrics = async () => {
+    const token = localStorage.getItem("token");
+    const map = {};
+    const commentResponse = await fetch(
+      "http://localhost:5000/feed/comments/metrics",
+      {
+        method: "GET",
+        headers: { "Content-Type": "application/json", token: token },
+      }
+    );
+    const commentData = await commentResponse.json();
+    for (let commentMetric of commentData) {
+      map[commentMetric.post_id] = [parseInt(commentMetric.commentcount), 0];
+    }
+    const likeResponse = await fetch("http://localhost:5000/feed/posts/likes", {
+      method: "GET",
+      headers: { "Content-Type": "application/json", token: token },
+    });
+    const likeData = await likeResponse.json();
+    for (let likeMetric of likeData) {
+      if (likeMetric.post_id in map) {
+        map[likeMetric.post_id][1] = parseInt(likeMetric.likecount);
+      } else {
+        map[likeMetric.post_id] = [0, parseInt(likeMetric.likecount)];
+      }
+    }
+    setFeedMetrics(map);
+  };
 
+  const getCommentMetrics = async () => {
+    const token = localStorage.getItem("token");
+    const map = {};
+
+    const response = await fetch("http://localhost:5000/feed/comments/likes", {
+      method: "GET",
+      headers: { "Content-Type": "application/json", token: token },
+    });
+    const commentLikesData = await response.json();
+    for (let commentLike of commentLikesData) {
+      map[commentLike.comment_id] = parseInt(commentLike.likecount);
+    }
+    setCommentMetrics(map);
+  };
   useEffect(() => {
     retrieveFeed();
+    getPostMetrics();
+    getCommentMetrics();
   }, []);
   return (
     <div className="container-fluid d-flex flex-column p-0">
