02108965d30ae677e945b246e6823283639def35
emir4ik-code <emirahmetov@gmail.com>
2022-10-25 22:40:09 -0400

    initial state

--- src/components/ToggleActivities/ToggleActivites.jsx
+++ src/components/ToggleActivities/ToggleActivites.jsx
@@ -1,33 +1,32 @@
-import React, {useState} from "react";
-import "./toggle.css"
+import React, { useState } from "react";
+import "./toggle.css";
 
 const ToggleActivites = () => {
-    const [toggle, setToggle] = useState();
+  const [toggle, setToggle] = useState("posts");
 
-
-    const handleClick = (e) => {
-        setToggle(e.target.id)
-    }
-    console.log(toggle);
+  const handleClick = (e) => {
+    setToggle(e.target.id);
+  };
+  console.log(toggle);
   return (
     <div className="d-flex flex-column">
-            <div className="d-flex align-self-center">
-                <h2>Activity</h2>
-            </div>
-            <div className="d-flex justify-content-around my-5">
-                <div className = "d-flex flex-column">
-                    <h5 id = "posts" onClick = {handleClick}>Posts</h5>
-                    {toggle === "posts" && (
-                        <div className = "line"></div>
-                    ) }
-                </div>
-            <div>
-                <h5 id = "comments" onClick = {handleClick}>Comments</h5>
-                {toggle === "comments" && (
-                        <div className = "line"></div>
-                    ) }
-            </div>
+      <div className="d-flex align-self-center">
+        <h2>Activity</h2>
+      </div>
+      <div className="d-flex justify-content-around my-5">
+        <div className="d-flex flex-column">
+          <h5 id="posts" onClick={handleClick}>
+            Posts
+          </h5>
+          {toggle === "posts" && <div className="line"></div>}
         </div>
+        <div>
+          <h5 id="comments" onClick={handleClick}>
+            Comments
+          </h5>
+          {toggle === "comments" && <div className="line"></div>}
+        </div>
+      </div>
     </div>
   );
 };
