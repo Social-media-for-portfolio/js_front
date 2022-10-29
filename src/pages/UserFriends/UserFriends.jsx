2a3a0394ac64f0b8952ea8f6fe3aa95169f52825
emir4ik-code <emirahmetov@gmail.com>
2022-10-29 15:36:23 -0400

    auth user can view friends, incoming and outgoing requests viewing other userFriends will only show friends

--- src/pages/UserFriends/UserFriends.jsx
+++ src/pages/UserFriends/UserFriends.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Navbar from "../../sections/Navbar";
 import Footer from "../../sections/Footer";
+import AuthContext from "../../context/authContext/AuthContext";
 import { useParams, useLocation } from "react-router-dom";
 import ToggleFriends from "../../components/ToggleFriends";
 
 const UserFriends = () => {
+  const {userInfo} = useContext(AuthContext);
   const { id } = useParams();
   const location = useLocation();
   const { userFriends } = location.state;
@@ -12,12 +14,18 @@ const UserFriends = () => {
   return (
     <div>
       <Navbar />
-      <ToggleFriends setToggleFriends={setToggleFriends} />
+      {userInfo.id === Number(id) && (
+      <><ToggleFriends setToggleFriends={setToggleFriends} />
       <div>
         {toggleFriends === "incoming" && <h2>Incoming</h2>}
         {toggleFriends === "outgoing" && <h2>Outgoing</h2>}
         {toggleFriends === "friends" && <h2>Friends</h2>}
       </div>
+      </>)}
+      {userInfo.id !== Number(id) && (
+        <h3>Friends</h3>
+      )}
+      
       <Footer />
     </div>
   );
