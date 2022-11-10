73cfcba8cf787646cc0f6e096e577dcefb919e6b
emir4ik-code <emirahmetov@gmail.com>
2022-11-10 14:01:40 -0500

    work work wokr

--- src/pages/Onboarding/Onboarding.jsx
+++ src/pages/Onboarding/Onboarding.jsx
@@ -1,27 +1,42 @@
-import React, {useState}from "react";
+import React, {useState, useEffect} from "react";
+import {Navigate} from "react-router-dom"
 import Navbar from "../../sections/Navbar/Navbar";
 import Footer from "../../sections/Footer/Footer";
 import WelcomeCard from "../../components/WelcomeCard";
 import InterestCard from "../../components/InterestCard";
+import { getMyUserInfo } from "../../utils/api";
 import "./onboarding.css";
 
 const Onboarding = () => {
+    const [name, setName] = useState("")
     const [interestsSelected, setInterestsSelected] = useState(0);
     const [error, setError] = useState(false);
+    const [navigate, setNavigate] = useState(false);
 
     const handleClick = () => {
         if(interestsSelected < 3) {
             setError(true)
             return;
         }
+        else {
+            setNavigate(true)
+        }
     }
+
+    const getUserInfo = async() => {
+        const {first_name} = await getMyUserInfo();
+        setName(first_name)
+    }
+    useEffect(() => {
+        getUserInfo();
+    }, [])
   return (
     <div className="contariner-fluid mx-0 px-0">
       <Navbar />
       <div className="d-flex flex-column align-items-center">
         <div className = "d-flex justify-content-center">
         <div className = "w-75 my-4">
-        <WelcomeCard />
+        <WelcomeCard firstName = {name}/>
         </div>
         </div>
       </div>
@@ -31,6 +46,9 @@ const Onboarding = () => {
             
             {error && (
                 <p className = "error align-self-center">You have to select at least 3 interests.</p>
+            )}
+            {navigate && (
+                <Navigate to ="/home"/>
             )}
         </div>
       <Footer />
